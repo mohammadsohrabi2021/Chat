0b0786dce51e0e24110afb22a153d605d32d7efd
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-15 11:54:50 +0330

    create components chats done

---
+++ src/Components/Chats/Chats.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState, useContext } from "react";
+import { auth } from "../../firebase";
+import { useNavigate } from 'react-router-dom';
+import { ChatEngine } from "react-chat-engine";
+import axios from "axios";
+
+// component
+import Navbar from "../Navbar/Navbar";
+
+// styles
+import styles from "./Chats.module.css";
+
+// context
+import { AuthContext } from "./../../Contexts/AuthContextProvider";
+
+const Chats = () => {
+  const [loading, setLoading] = useState(true);
+  const user = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/');
+      return;
+    }
+    axios
+      .get("https://api.chatengine.io/users/me", {
+        headers: {
+          "project-id": "a9b6b474-198b-4dc5-bb35-bc5492c231d2",
+          "user-name": user.email,
+          "user-secret": user.uid,
+        },
+      })
+      .then(() => {
+        setLoading(false);
+      })
+      .catch(() => {
+        let formdata = new FormData();
+        formdata.append("email", user.email);
+        formdata.append("username", user.email);
+        formdata.append("secret", user.uid);
+        getFile(user.photoURL).then(avatar => {
+          formdata.append("avatar", avatar, avatar.name);
+          axios
+            .post("https://api.chatengine.io/users/", formdata, {
+              headers: {
+                "private-key": "a57b8767-610e-4a1e-b3a9-d93ff3807079",
+              },
+            })
+            .then(() => setLoading(false))
+            .catch((error) => console.log(error));
+        });
+      });
+  }, [user,navigate]);
+
+  const getFile = async (url) => {
+    const response = await fetch(url);
+    const data = await response.blob();
+    return new File([data], "userPhoto.jpg", { type: "image/jpeg" });
+  };
+  const logoutHandler = async () => {
+    await auth.signOut();
+    navigate('/');
+
+  };
+
+  if (!user || loading) return "Loading ...";
+
+  return (
+    <div className={styles.container}>
+      <Navbar logoutHandler={logoutHandler} />
+      <ChatEngine
+        height="calc(100vh -50px)"
+        projectID="a9b6b474-198b-4dc5-bb35-bc5492c231d2"
+        userName={user.email}
+        userSecret={user.uid}
+      />
+    </div>
+  );
+};
+
+export default Chats;
