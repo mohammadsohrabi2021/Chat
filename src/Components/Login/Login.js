15ed09b125356640665569013f370c72565522d8
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-15 11:55:48 +0330

    refactor login components done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,22 +1,23 @@
 import React from "react";
 import firebase from 'firebase/compat/app';
 import { auth } from "../../firebase";
+
 // icon
 import google from "../../assets/Image/google.svg";
+
 // style
 import styles from "./Login.module.css";
+
 const Login = () => {
   return (
     <div className={styles.loginPage}>
       <div className={styles.loginCard}>
         <h2>Welcome to Chat!</h2>
         <div
-          onClick={() =>
-            auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
-          }
+          onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
           className={styles.button}
         >
-          <img src={google} alt="giigle" /> Sign in with Google
+          <img src={google} alt="google" /> Sign in with Google
         </div>
       </div>
     </div>
