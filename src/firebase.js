64a8373355283b9285c3a83c732ee4a3b0c134a3
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-12-22 23:18:57 +0330

    refactor firebase done

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,14 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
-export  const auth = firebase.initializeApp({
-    apiKey: "AIzaSyDN439IQOiLv_3_e490HRcwHa0eOtzDkzw",
-    authDomain: "chat-fcc5e.firebaseapp.com",
-    projectId: "chat-fcc5e",
-    storageBucket: "chat-fcc5e.appspot.com",
-    messagingSenderId: "483890828800",
-    appId: "1:483890828800:web:3816ac831a24b09acbaae5"
-  }).auth();
+export const auth = firebase
+  .initializeApp({
+    apiKey: "AIzaSyAQr3a3msfOrKDjFUo8G2E5sp1JOo1IVCY",
+    authDomain: "chatmohammad.firebaseapp.com",
+    projectId: "chatmohammad",
+    storageBucket: "chatmohammad.appspot.com",
+    messagingSenderId: "225757995350",
+    appId: "1:225757995350:web:baf80745a930d9c9de76f7",
+    measurementId: "G-97VK1NG3KB",
+  })
+  .auth();
